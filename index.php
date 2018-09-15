b975d81dfbbb3418a678aeb18624b7db3dac23d8
Javier Diaz Chourio <javier.diazch@gmail.com>
2018-09-15 21:08:51 +0200

    Trying to solve encoding problem

--- index.php
+++ index.php
@@ -1,2 +1,4 @@
-<?php header('Content-Type: text/html');
-<?php include_once("opera/francesca-da-rimini/act1.html"); ?>
+<?php 
+header('Content-Type: text/html');
+include_once("opera/francesca-da-rimini/act1.html"); 
+?>
